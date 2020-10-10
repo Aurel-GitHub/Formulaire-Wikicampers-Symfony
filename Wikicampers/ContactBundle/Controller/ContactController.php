a0c3433f7b1c0fb2d4475121e338932e0986c07e
@Aurel-GitHub <aurelien.landouer@hotmail.fr>
2020-10-10 18:09:48 +0200

    Bundle first Hello World

--- Wikicampers/ContactBundle/Controller/ContactController.php
+++ Wikicampers/ContactBundle/Controller/ContactController.php
@@ -10,11 +10,14 @@ class ContactController extends Controller
 {
     public function homeAction()
     {
-        return $this->render('WikicampersContactBundle:Default:index.html.twig');
+//        return $this->render('WikicampersContactBundle:Default:index.html.twig');
+        return $this->render('@WikicampersContact/Default/index.html.twig');
+
     }
 
     public function formAction()
     {
+        return $this->render('@WikicampersContact/Default/index.html.twig');
 
     }
 
